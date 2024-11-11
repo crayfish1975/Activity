ce0c244395af373403860ce7c9a00228c486f346
crayfish1975 <sermin@bk.ru>
2024-11-11 18:29:47 +0400

    Activity #2 доделал

--- receiver/src/main/java/otus/gpb/homework/activities/receiver/ReceiverActivity.kt
+++ receiver/src/main/java/otus/gpb/homework/activities/receiver/ReceiverActivity.kt
@@ -10,5 +10,18 @@ class ReceiverActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_receiver)
+
+        val title = intent.getStringExtra("title")
+        findViewById<TextView>(R.id.titleTextView).text = title
+        findViewById<TextView>(R.id.descriptionTextView).text = intent.getStringExtra("description")
+        findViewById<TextView>(R.id.yearTextView).text = intent.getStringExtra("year")
+
+        findViewById<ImageView>(R.id.posterImageView).setImageResource(
+            when (title) {
+                "Славные парни" -> R.drawable.niceguys
+                "Интерстеллар" -> R.drawable.interstellar
+                else -> R.drawable.interstellar
+            }
+        )
     }
 }
